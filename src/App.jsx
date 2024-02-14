b3aa081f7a76e2ebfcb40c3328f8f844b8a3bb86
Nikola Zovko <nikolajoe95@gmail.com>
2024-02-14 15:44:43 +0100

    novi updejt

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Header from './components/Header'
 import Content from './components/Content'
 import Content2 from './components/Content2'
+import Content3 from './components/Content3'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,7 +16,7 @@ function App() {
       <Header />
       <Content />
       <Content2 />
-
+      <Content3 />
 
     </div>
   )
---
+++ src/components/Content3.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Content3 = () => {
+    return (
+        <div>
+            <h1>Content 3</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia fugiat ad placeat impedit iure consequatur praesentium porro repellendus voluptates alias. Odio ut voluptatibus hic amet expedita, aperiam ad! Est, dolor! Incidunt eveniet, illo facere et corporis repudiandae iure alias ex.</p>
+        </div>
+    )
+}
+
+export default Content3
